969979dd6796b63d33bb2661009334a08a1e0729
FernanCastillo <fern.castillop@duocuc.cl>
2025-10-20 19:17:55 -0300

    Primer commit - Esqueleto de la página

---
+++ src/utils/localstorageHelper.js
@@ -0,0 +1,24 @@
+export const saveLocalstorage = (key,data) => {
+    try {
+        localStorage.setItem(key, JSON.stringify(data))
+    } catch (ex) {
+        console.error("Error al guardar: ",ex)
+    }
+}
+
+export const loadFromLocalstorage = (key) => {
+    try {
+        const data = localStorage.getItem(key)
+        return data ? JSON.parse(data) : null
+    } catch (ex) {
+        console.error("Error al guardar: ",ex)
+    }
+}
+
+export const deleteFromLocalstorage = (key) => {
+    try {
+        localStorage.removeItem(key)
+    } catch (ex) {
+        console.error("Error al guardar: ",ex)
+    }
+}
